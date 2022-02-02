4dffbc84d6db09e5fa8a433c75d7833fb94129bf
Anthony Luo <anthonyluo100@Gmail.com>
2022-02-02 08:34:23 -0500

    have some amount of visualization

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
 # SpaceRyde Internship Coding Challenge
+# main.py
 # Februry 2022
 # Anthony Luo, a26luo@uwaterloo.ca / antholuo@gmail.com
 
 import math
 import pygame
 
-# Define global constants here
-TRAFFIC_MIN_DIST = 100  # minimum distance between airplanes, in meters (== 100m)
-ATC_RADIUS = 10000  # traffic control zone radius, in meters        (== 10km)
-HOLD_RADIUS = 1000  # holding pattern radius                        (== 1km)
-
-AIRSPEED = 140  # traffic airspeed, constant, in meters/second  (== 140m/s)
-
-# RUNWAY CONSTANTS.
-NUM_RUNWAYS = 2  # there are two runways
-RUNWAY_SPACING = 500  # distance between runways, inside edge         (== 500m)
-RUNWAY_LENGTH = 500  # length of runway, in meters                   (== 500m)
-RUNWAY_WIDTH = 100  # width of runway, in meters                    (== 100m)
+from consts import * # there should be nothing BUT global constants here
 
 """Some notes to myself:
 - 10hz = 10 times per second
