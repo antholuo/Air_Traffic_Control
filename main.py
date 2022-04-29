abc5c195ef80fd4c4ba32e2935e4781a8a4c4c4a
Anthony Luo <anthonyluo100@Gmail.com>
2022-04-29 17:15:44 -0400

    Basic ball visualization

---
+++ main.py
@@ -0,0 +1,21 @@
+########################################
+# main.py
+#########
+# ATC Challenge
+# Anthony Luo, January 2022
+#########
+# Runs everything in different threads
+########################################
+
+import threading
+
+def start_atc():
+    # visualization = threading.Thread(target=run_visualization, args=())
+
+    # visualization.start();
+
+    # visualization.join(); # I don't think that we need to join them..
+    return
+
+if __name__ == "__main__":
+    start_atc();
