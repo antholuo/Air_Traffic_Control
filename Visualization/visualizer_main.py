b39ee36282ea0306dc4d9d363fad31d4d775a425
Anthony Luo <anthonyluo100@Gmail.com>
2022-04-29 22:18:52 -0400

    circle

--- Visualization/visualizer_main.py
+++ Visualization/visualizer_main.py
@@ -4,10 +4,10 @@ import sys
 
 def run_visualization():
     pygame.init()
-    size = width, height = 320, 240
+    size = width, height = 1200, 1200
     speed = [2, 2]
     black = 0, 0, 0
-    screen = pygame.display.set_mode(size)
+    window = pygame.display.set_mode(size)
 
     ball = pygame.image.load("intro_ball.gif")
     ballrect = ball.get_rect()
@@ -20,8 +20,11 @@ def run_visualization():
             speed[0] = -speed[0]
         if ballrect.top < 0 or ballrect.bottom > height:
             speed[1] = -speed[1]
-        screen.fill(black)
-        screen.blit(ball, ballrect)
+
+        window.fill(black)
+        window.blit(ball, ballrect)
+        pygame.draw.circle(window, (0, 255, 0),
+                           [600, 600], 600, 3)
         pygame.display.flip()
     return
 
