5d3cadc008ef9f40ad7b44373544cb42bf0e7d5f
Anthony Luo <anthonyluo100@Gmail.com>
2022-04-30 08:43:36 -0400

    Add 2-runway generation

--- Airfield/airfield.py
+++ Airfield/airfield.py
@@ -6,20 +6,28 @@ class Runway:
         self.y = center_y
         self.width = width
         self.length = length
-        self.x_edges = (center_x - width/2, center_x + width/2)
-        self.y_edges = (center_y - length/2, center_y + length/2)
+        self.x_edges = (center_x - width / 2, center_x + width / 2)
+        self.y_edges = (center_y - length / 2, center_y + length / 2)
+
 
 class Airfield:
-    def __init__(self, radius, num_runways=2, runway_length=1000, runway_width=500, runway_spacing = 500):
-        self.num_runways = num_runways # load this from configuration file later
+    def __init__(self, radius, num_runways=2, runways=[], runway_length=1000, runway_width=500, runway_spacing=500):
+        self.num_runways = num_runways  # load this from configuration file later
         self.radius = radius
         self.runway_length = runway_length
         self.runway_width = runway_width
+        self.runway_spacing = runway_spacing
+        # todo: import a bunch of predefined runways
         self.runways = self.generate_runways()
 
     def generate_runways(self):
         # generates runways and returns some data
         runways = []
-        for i in range(0, self.num_runways):
-            runways.append(i)
+        # currently only generates the two runways
+        runway_r = Runway(self.runway_width / 2 + self.runway_spacing / 2, 0, self.runway_width, self.runway_length)
+        runway_l = Runway(- self.runway_width / 2 - self.runway_spacing / 2, 0, self.runway_width, self.runway_length)
+
+        runways.append(runway_r)
+        runways.append(runway_l)
+        # TODO: make them generate automagically
         return runways
