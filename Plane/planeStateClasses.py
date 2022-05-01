bc40ef52489191ee156a79822abee22f1699c299
Anthony Luo <anthonyluo100@Gmail.com>
2022-05-01 10:11:15 -0400

    updatePosition() while Flying now updates & returns lat/lon properly

--- Plane/planeStateClasses.py
+++ Plane/planeStateClasses.py
@@ -1,5 +1,8 @@
 from plane import Plane
 from plane import State
+from Utils.ATC_CONSTS import *
+import math
+
 
 # i really have no clue how to make a state machine
 
@@ -7,19 +10,39 @@ class Flying(State):
     def updateHeading(self, heading: float) -> None:
         self.plane.heading = heading
 
-    def updatePosition(self, delta_seconds: float=1):
+    def updatePosition(self, delta_seconds: float = 0.1) -> (float, float, float):
         # calculate how far we have flown in delta time.
-        newlat = 0
-        newlon = 0
+        # also updates the planes position
+
+        # returns updated position in a straight line.
+        lat = math.radians(self.plane.lat)
+        lon = math.radians(self.plane.lon)
+        dist = PLANE_AIRSPEED * delta_seconds
+
         heading = self.plane.heading
+
+        newlat = math.asin(
+            math.sin(lat) * math.cos(dist / EARTH_RADIUS) + math.cos(lat) * math.sin(dist / EARTH_RADIUS) * math.cos(
+                heading))
+        newlon = lon + math.atan2(math.sin(heading) * math.sin(dist / EARTH_RADIUS) * math.cos(lat),
+                                  math.cos(dist / EARTH_RADIUS) - math.sin(lat) * math.sin(newlat))
+        heading = self.plane.heading
+
         return newlat, newlon, heading
+
 
 class Holding(State):
     def updateHeading(self, heading: float) -> None:
         self.plane.heading = heading
         self.plane.setState(Flying())
 
-    def updatePosition(self, delta_seconds: float=1) -> (float, float, float):
+    def updatePosition(self, delta_seconds: float = 0.1) -> (float, float, float):  # hope default val is ok
+        # update position after circling around.
+
+        # first calculate how far it is possible to fly, then turn that into arclength
+        # calculate triangle with original position, arclength.
+        # get new heading
+        # get new position
         newlat = 0
         newlon = 0
         heading = self.plane.heading
