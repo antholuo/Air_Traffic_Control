695fb46d4b7249b214bdf983309595dcb46198c5
Anthony Luo <anthonyluo100@gmail.com>
2026-09-01 18:32:18 -0700

    Add macroquad and try centering some text

--- atc_main/src/main.rs
+++ atc_main/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+fn draw_centered_text(text: &str, center_x: f32, center_y: f32, font_size: f32, color: Color) {
+    let text_dims = measure_text(text, None, font_size as u16, 1.0);
+
+    // minus half-width for horizontal center
+    let x = center_x - text_dims.width / 2.0;
+    let y = center_y + text_dims.height / 2.0;
+
+    draw_text(text, x, y, font_size, color);
+}
+
+#[macroquad::main("AtcMain")]
+async fn main() {
+    loop {
+        clear_background(GRAY);
+
+        let screen_center_x = screen_width() / 2.0;
+        let screen_center_y = screen_height() / 2.0;
+
+        let greeting = "Goofy Goomba";
+        let font_size = 32.0;
+
+        draw_centered_text(greeting, screen_center_x, screen_center_y, font_size, WHITE);
+
+        // draw_line(x1, y1, x1, y2, thickness, colour);
+        draw_line(0.0, 0.0, 100.0, 100.0, 10.0, BLUE);
+        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 12.0, 60.0, GREEN);
+
+        // draw_text (text, x, y, font_size, colour)
+        // where x, y is bottom-left baseline anchor
+        draw_text("Hello, Macrquad!", 20.0, 20.0, 30.0, WHITE);
+
+        next_frame().await
+    }
 }
