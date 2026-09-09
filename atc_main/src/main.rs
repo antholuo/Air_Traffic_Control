e711e554428c69642b26ed1d62d00e4520469d34
Anthony Luo <anthonyluo100@gmail.com>
2026-09-08 18:06:44 -0700

    basic rotation

--- atc_main/src/main.rs
+++ atc_main/src/main.rs
@@ -1,4 +1,5 @@
 use macroquad::prelude::*;
+use trig::Trig;
 
 fn draw_centered_text(text: &str, center_x: f32, center_y: f32, font_size: f32, color: Color) {
     let text_dims = measure_text(text, None, font_size as u16, 1.0);
@@ -12,15 +13,35 @@ fn draw_centered_text(text: &str, center_x: f32, center_y: f32, font_size: f32, 
     draw_text(text, x, y, font_size, color);
 }
 
+fn deg_to_coord(radius: f32, degrees: f32) -> (f32, f32) {
+    // Given SOH CAH TOA
+    // x = cos(degrees) * radius
+    // y = sin(degrees) * radius
+    (
+        degrees.to_radians().cos() * radius,
+        degrees.to_radians().sin() * radius,
+    )
+}
+
 #[macroquad::main("AtcMain")]
 async fn main() {
+    let cam_radius: f32 = 80.0;
+    let cam_y: f32 = 50.0;
+    let cam_rotation_dps: f32 = 2.0;
+    let mut cam_rotation_angle: f32 = 0.0;
     loop {
         clear_background(LIGHTGRAY);
+
+        // calculate camera rotation
+        cam_rotation_angle += cam_rotation_dps * get_frame_time(); // Rotate N degrees per second
+        //
+
+        let (cam_x, cam_z) = deg_to_coord(cam_radius, cam_rotation_angle);
 
         // Set the camera
         // Note: I think position is x/y/z?
         set_camera(&Camera3D {
-            position: vec3(50.0, 50.0, -50.0),
+            position: vec3(cam_x, cam_y, cam_z),
             target: vec3(0.0, 0.0, 0.0),
             up: vec3(0.0, 1.0, 0.0),
             ..Default::default() // rest of the fields that I'm not gonna bother with for now
